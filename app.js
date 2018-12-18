21f9d4d02d6bf64157ce7e262bcb37e05f659330
Maxime COLLIN <33457385+MaxCo41@users.noreply.github.com>
2018-12-18 15:43:24 +0100

    Version Ok

--- app.js
+++ app.js
@@ -1,48 +1,92 @@
-var express = require('express');
+var app = require('express')();
 var session = require('cookie-session'); // Charge le middleware de sessions
 var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var ent = require('ent');
 
-var app = express();
+var todolist = [];
 
 
-/* On utilise les sessions */
-app.use(session({secret: 'todotopsecret'}))
+
+
+
+//Affichage de la liste et du formulaire
+app.get('/', function(req, res){
+    res.render('todo.ejs', {todolist: todolist});
+})
+
+// Redirige vers la todolist si page demandée pas trouvé
+app.use(function(req, res, next){
+    res.redirect('/todo');
+})
+
+//echange avec socket.io
+io.sockets.on('connection', function(socket){
+    /* console.log('User is connected'); */
+
+    // Envoi de la todo en temps réel
+    socket.emit('todolist', todolist);
+
+    //ajout d'une todo à la liste
+    socket.on('addTodo', function(todo){
+        console.log(todo);
+        
+        todo = ent.encode(todo);
+        todolist.push(todo);
+        
+        index = todolist.length -1;
+
+        //Envoi à tous les users
+        socket.broadcast.emit('addTodo', {todo: todo, index:index});
+    });
+
+    //Delete Todo
+    socket.on('deleteTodo', function(index){
+        todolist.splice(index, 1);
+        io.sockets.emit('todolist', todolist);
+    });
+    
+});
+
+
+
+server.listen(8080);
 
 
 /* S'il n'y a pas de todolist dans la session,
 on en crée une vide sous forme d'array avant la suite */
-.use(function(req, res, next){
+/* .use(function(req, res, next){
     if (typeof(req.session.todolist) == 'undefined') {
         req.session.todolist = [];
     }
     next();
-})
+}) */
 
 /* On affiche la todolist et le formulaire */
-.get('/todo', function(req, res) { 
+/* .get('/todo', function(req, res) { 
     res.render('todo.ejs', {todolist: req.session.todolist});
-})
+}) */
 
 /* On ajoute un élément à la todolist */
-.post('/todo/ajouter/', urlencodedParser, function(req, res) {
-    if (req.body.newtodo != '') {
-        req.session.todolist.push(req.body.newtodo);
+/* .post('/todo/ajouter/', urlencodedParser, function(req, res) {
+    if (req.body.newTodo != '') {
+        req.session.todolist.push(req.body.newTodo);
     }
     res.redirect('/todo');
-})
+}) */
 
 /* Supprime un élément de la todolist */
-.get('/todo/supprimer/:id', function(req, res) {
+/* .get('/todo/supprimer/:id', function(req, res) {
     if (req.params.id != '') {
         req.session.todolist.splice(req.params.id, 1);
     }
     res.redirect('/todo');
-})
+}) */
 
 /* On redirige vers la todolist si la page demandée n'est pas trouvée */
-.use(function(req, res, next){
+/* .use(function(req, res, next){
     res.redirect('/todo');
-})
+}) */
 
-.listen(8080);
